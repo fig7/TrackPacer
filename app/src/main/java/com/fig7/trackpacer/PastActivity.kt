f7bb7bc4603f84d8e9de9fc4e35510a02bab2958
Stuart Fisher <stuart.fisher@fig7.com>
2023-10-31 18:42:18 +0000

    Try again.

--- app/src/main/java/com/fig7/trackpacer/PastActivity.kt
+++ app/src/main/java/com/fig7/trackpacer/PastActivity.kt
@@ -4,11 +4,11 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.BundleCompat
 import com.fig7.trackpacer.data.ResultData
 import com.fig7.trackpacer.data.ResultModel
 import com.fig7.trackpacer.data.StatusModel
 import com.fig7.trackpacer.databinding.ActivityPastBinding
-
 
 class PastActivity: AppCompatActivity() {
     private lateinit var binding: ActivityPastBinding
@@ -30,7 +30,7 @@ class PastActivity: AppCompatActivity() {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             initData.classLoader   = ResultData::class.java.classLoader
-            resultModel.resultData = initData.getParcelable("ResultParcel", ResultData::class.java)!!
+            resultModel.resultData = BundleCompat.getParcelable(initData, "ResultParcel", ResultData::class.java)!!
         } else {
             @Suppress("DEPRECATION")
             resultModel.resultData = initData.getParcelable("ResultParcel")!!
